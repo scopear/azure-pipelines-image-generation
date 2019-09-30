5b9cb12ad1e8d40a245b1d2de66b060891a2b2a8
Nathan Fok <nathan@scopear.com>
2019-09-30 16:53:59 -0600

    Added MFC components to 2017 install, added script to install win10sdk 18362

---
+++ images/win/scripts/Installers/Vs2017/Install-Win10SDK18362.ps1
@@ -0,0 +1,15 @@
+################################################################################
+##  File:  Install-Win10SDK18362.ps1
+##  Team:  CI-Platform
+##  Desc:  Install Windows 10 SDK (10.0.18362.0)
+################################################################################
+
+Import-Module -Name ImageHelpers -Force
+
+$InstallerURI = 'https://download.microsoft.com/download/4/2/2/42245968-6A79-4DA7-A5FB-08C0AD0AE661/windowssdk/winsdksetup.exe'
+$InstallerName = 'winsdksetup.exe'
+$ArgumentList = ('/features', '/quiet', '/norestart')
+
+$exitCode = Install-EXE -Url $InstallerURI -Name $InstallerName -ArgumentList $ArgumentList
+
+exit $exitCode
